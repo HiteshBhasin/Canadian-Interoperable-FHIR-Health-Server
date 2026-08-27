9355931c28dbc07f9b177420fd0ae2e2d3ad943c
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2026-08-27 09:25:24 -0500

    new commit

---
+++ fhir_healthcare/src/main/java/com/cnfhir/fhir_healthcare/FhirHealthcareApplication.java
@@ -0,0 +1,13 @@
+package com.cnfhir.fhir_healthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhirHealthcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhirHealthcareApplication.class, args);
+	}
+
+}
---
+++ fhir_healthcare/src/test/java/com/cnfhir/fhir_healthcare/FhirHealthcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnfhir.fhir_healthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FhirHealthcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
